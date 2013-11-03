2b80dbae928c885cc1366589b02a43683daf51cc
dariushha <b.g.dariush@gmail.com>
2013-11-03 21:42:44 +0330

    + authModule

---
+++ modules/authModule/authBootstrap.php
@@ -0,0 +1,22 @@
+<?php
+namespace modules\authModule;
+/**
+* The authModule's Bootstrapper
+*/
+class authBootstrap
+{
+    /**
+     * A pre-dispatch function
+     * @param \zinux\kernel\routing\request $request
+     */
+    public function pre_CHECK(\zinux\kernel\routing\request $request)
+    {
+    }
+    /**
+     * A post-dispatch function
+     * @param \zinux\kernel\routing\request $request
+     */
+    public function post_CHECK(\zinux\kernel\routing\request $request)
+    {
+    }
+}
---
+++ modules/authModule/controllers/indexController.php
@@ -0,0 +1,35 @@
+<?php
+namespace modules\authModule\controllers;
+    
+/**
+ * The modules\authModule\controllers\indexController
+ * @by Zinux Generator <b.g.dariush@gmail.com>
+ */
+class indexController extends \zinux\kernel\controller\baseController
+{
+    /**
+    * The modules\authModule\controllers\indexController::IndexAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function IndexAction()
+    {
+    }
+
+    /**
+    * The \modules\authModule\controllers\indexController::loginAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function loginAction()
+    {
+        
+    }
+
+    /**
+    * The \modules\authModule\controllers\indexController::logoutAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function logoutAction()
+    {
+        
+    }
+}
