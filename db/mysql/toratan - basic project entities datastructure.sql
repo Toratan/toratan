9d899a6eb82739148c578ac0f157e6bd1f54f902
dariush <b.g.dariush@gmail.com>
2013-11-19 23:16:05 +0330

    zinux 3.1.5 is up

--- db/mysql/toratan - basic project entities datastructure.sql
+++ db/mysql/toratan - basic project entities datastructure.sql
@@ -1,174 +1,202 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
 --
--- Host: localhost
--- Generation Time: Nov 18, 2013 at 11:07 PM
--- Server version: 5.5.34
--- PHP Version: 5.3.10-1ubuntu3.8
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: toratan
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `toratan`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `folders`
 --
 
 DROP TABLE IF EXISTS `folders`;
-CREATE TABLE IF NOT EXISTS `folders` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `folders` (
   `folder_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `parent_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `owner_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `folder_title` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `folder_body` bit(1) DEFAULT NULL,
-  `is_public` bit(1) NOT NULL DEFAULT b'0',
+  `is_public` tinyint(1) NOT NULL DEFAULT '0',
+  `is_trash` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`folder_id`),
   KEY `user_id` (`owner_id`),
-  KEY `parent_id` (`parent_id`)
+  KEY `parent_id` (`parent_id`),
+  CONSTRAINT `folders_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `folders_ibfk_2` FOREIGN KEY (`parent_id`) REFERENCES `folders` (`folder_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
-
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Triggers `folders`
+-- Dumping data for table `folders`
 --
-DROP TRIGGER IF EXISTS `T_FOLDERS_PREVENT_ROOT_DELETION`;
-DELIMITER //
-CREATE TRIGGER `T_FOLDERS_PREVENT_ROOT_DELETION` BEFORE DELETE ON `folders`
- FOR EACH ROW BEGIN
-  IF OLD.folder_id = 0 OR OLD.owner_id = 0 THEN 
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'cannot DELETE ROOT owing items';
+
+LOCK TABLES `folders` WRITE;
+/*!40000 ALTER TABLE `folders` DISABLE KEYS */;
+INSERT INTO `folders` VALUES ('0','0','0','ROOT',NULL,0,0,'2013-11-19 21:21:53','2013-11-19 21:21:53');
+/*!40000 ALTER TABLE `folders` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER T_FOLDERS_PREVENT_ROOT_DELETION BEFORE DELETE ON folders FOR EACH ROW
+BEGIN
+  IF OLD.owner_id = '0' THEN 
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'CANNOT DELETE ROOT OWNING ITEMS';
   END IF;
-END
-//
+END */;;
 DELIMITER ;
-
--- --------------------------------------------------------
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `links`
 --
 
 DROP TABLE IF EXISTS `links`;
-CREATE TABLE IF NOT EXISTS `links` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `links` (
   `link_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `parent_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `owner_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `link_title` varchar(300) COLLATE utf8_persian_ci NOT NULL,
   `link_body` text COLLATE utf8_persian_ci NOT NULL,
-  `is_public` bit(1) NOT NULL DEFAULT b'0',
+  `is_public` tinyint(1) NOT NULL DEFAULT '0',
+  `is_trash` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`link_id`),
   KEY `parent_id` (`parent_id`,`owner_id`),
-  KEY `owner_id` (`owner_id`)
+  KEY `owner_id` (`owner_id`),
+  CONSTRAINT `links_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `folders` (`folder_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `links_ibfk_2` FOREIGN KEY (`owner_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `links`
+--
+
+LOCK TABLES `links` WRITE;
+/*!40000 ALTER TABLE `links` DISABLE KEYS */;
+/*!40000 ALTER TABLE `links` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `notes`
 --
 
 DROP TABLE IF EXISTS `notes`;
-CREATE TABLE IF NOT EXISTS `notes` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notes` (
   `note_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `parent_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `owner_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `note_title` varchar(300) COLLATE utf8_persian_ci NOT NULL,
   `note_body` text COLLATE utf8_persian_ci NOT NULL,
-  `is_public` bit(1) NOT NULL DEFAULT b'0',
+  `is_public` tinyint(1) NOT NULL DEFAULT '0',
+  `is_trash` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`note_id`),
   KEY `parent_id` (`parent_id`,`owner_id`),
-  KEY `owner_id` (`owner_id`)
+  KEY `owner_id` (`owner_id`),
+  CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `folders` (`folder_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `notes_ibfk_2` FOREIGN KEY (`owner_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `notes`
+--
+
+LOCK TABLES `notes` WRITE;
+/*!40000 ALTER TABLE `notes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `user_id` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `email` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `username` varchar(250) COLLATE utf8_persian_ci NOT NULL,
   `password` varchar(250) COLLATE utf8_persian_ci NOT NULL,
+  `is_deactive` bit(1) NOT NULL DEFAULT b'0',
+  `deactivate_at` bit(1) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `email`, `username`, `password`, `created_at`, `updated_at`) VALUES
-('0', 'NULL', 'ROOT', 'NULL', '2013-11-18 22:22:08', '2013-11-18 22:22:08');
-
---
--- Dumping data for table `folders`
---
-
-INSERT INTO `folders` (`folder_id`, `parent_id`, `owner_id`, `folder_title`, `folder_body`, `is_public`, `created_at`, `updated_at`) VALUES
-('0', '', '0', 'ROOT', NULL, b'0', '2013-11-18 22:22:30', '2013-11-18 22:22:30');
-
---
--- Triggers `users`
---
-DROP TRIGGER IF EXISTS `T_USERS_PREVENT_ROOT_DELETION`;
-DELIMITER //
-CREATE TRIGGER `T_USERS_PREVENT_ROOT_DELETION` BEFORE DELETE ON `users`
- FOR EACH ROW BEGIN
-  IF OLD.user_id = 0 THEN 
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'cannot DELETE ROOT user';
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('0','NULL','ROOT','NULL','\0',NULL,'2013-11-18 22:22:08','2013-11-18 22:22:08'),('0d01e5d6df681a262fa493130d9dfb04','b.g.dariush@gmail.com','badguy','43fbb4a472eb5a9ec6085e2b1217dd0e','\0',NULL,'2013-11-19 10:52:14','2013-11-19 10:52:14');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER T_USERS_PREVENT_ROOT_DELETION BEFORE DELETE ON users FOR EACH ROW
+BEGIN
+  IF OLD.user_id = '0' THEN 
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'CANNOT DELETE ROOT USER';
   END IF;
-END
-//
+END */;;
 DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `folders`
---
-ALTER TABLE `folders`
-  ADD CONSTRAINT `folders_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `links`
---
-ALTER TABLE `links`
-  ADD CONSTRAINT `links_ibfk_2` FOREIGN KEY (`owner_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `links_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `folders` (`folder_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `notes`
---
-ALTER TABLE `notes`
-  ADD CONSTRAINT `notes_ibfk_2` FOREIGN KEY (`owner_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `folders` (`folder_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-11-19 23:07:58
