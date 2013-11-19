e51fe6f6651e53cfe82bebcb7940895cec43c28e
dariush <b.g.dariush@gmail.com>
2013-11-19 23:12:52 +0330

    @ item : newItem/edit/delete/fetch/fetchItems improved or added @ folder : newItem/delete improved or added + dbNotFound

---
+++ core/db/exceptions/dbNotFound.php
@@ -0,0 +1,6 @@
+<?php
+namespace core\db\exceptions;
+
+class dbNotFound extends \zinux\kernel\exceptions\dbException
+{
+}
--- core/db/models/folder.php
+++ core/db/models/folder.php
@@ -14,6 +14,35 @@ class folder extends item
      */
     public function newItem($folder_name, $parent_id, $owner_id)
     {
-       parent::newItem($folder_name, NULL, $parent_id, $owner_id);
+       return parent::newItem($folder_name, NULL, $parent_id, $owner_id);
+    }
+    /**
+     * Edit a folder
+     * @param string $item_id the item's ID
+     * @param string $owner_id the item's owner's ID 
+     * @param string $folder_name the new folder's name
+     * @param boolean $is_public should it mark as public folder, assign to -1 to leave it as is
+     * @param boolean $is_trash should it mark as trashed folder, assign to -1 to leave it as is
+     */
+    public function edit($item_id, $owner_id, $folder_name, $is_public = -1, $is_trash = -1)
+    {
+        # delete the item, because we are going re-generate the item's ID
+        $deleted_item = parent::delete($item_id, $owner_id, 0);
+        # creates a new item 
+        $item = $this->newItem($folder_name, $deleted_item->parent_id, $owner_id);
+        # modify the publicity of the item if necessary        
+        if($is_public==-1)
+            $item->is_public = $deleted_item->is_public;
+        else
+            $item->is_public = $is_public;
+        # modify the trash flag of the item if necessary
+        if($is_trash==-1)
+            $item->is_trash = $deleted_item->is_trash;
+        else
+            $item->is_trash = $is_trash;
+        # save the item
+        $item->save();
+        # return the edited item
+        return $item;
     }
 }
