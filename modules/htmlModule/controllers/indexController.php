95d64855afbdece5783d759f99cd8de2a6b8900f
dariushha <b.g.dariush@gmail.com>
2013-11-20 21:17:18 +0330

    + /archives + /trashes added

--- modules/htmlModule/controllers/indexController.php
+++ modules/htmlModule/controllers/indexController.php
@@ -13,39 +13,46 @@ class indexController extends \zinux\kernel\controller\baseController
     */
     public function IndexAction()
     {
-        if(array_key_exists("clear_session",$this->request->params))
-        {
-            echo "SESSION CLEARED";
-            session_destroy();
-            unset($_SESSION);
-        }
+        if(!\core\db\models\user::IsSignedin()) return;
         $this->layout->AddTitle("Home");
-        $this->view->users = \core\db\models\user::all();
-        if(array_key_exists("show_session",$this->request->params))
-        {
-            if(\core\db\models\user::IsSignedin())
-            {
-                echo "YOU ARE SIGNED IN";
-                \zinux\kernel\utilities\debug::_var(\core\db\models\user::GetInstance());
-            }
-            \zinux\kernel\utilities\debug::_var($_SESSION);
-        }
         $f = new \core\db\models\folder();
-//        \zinux\kernel\utilities\debug::_var($f->fetch("0a52819e8266130877dea08a6622f0fe", \core\db\models\user::GetInstance()->user_id."A"));
-        #$f->addAnItem();
-        echo "<div>";
-        echo "ALL {$f->whoami()} count : {$f->count()}";
-        echo "</div>";        
         if(!isset($this->request->params["directory"]))
             $this->request->params["directory"] = 0;
         $this->view->pid = $pid = $this->request->params["directory"];
         $uid = \core\db\models\user::GetInstance()->user_id;
-        if($pid)
-            $this->view->cwd = $f->fetch($pid, $uid);
         $this->view->folders = ($f->fetchItems($pid, $uid, -1, 0));
         $n = new \core\db\models\note;
         $this->view->notes = ($n->fetchItems($pid, $uid, -1, 0));
         $l = new \core\db\models\link;
         $this->view->links = ($l->fetchItems($pid, $uid, -1, 0));
+    }
+
+    /**
+    * The \modules\htmlModule\controllers\indexController::trashesAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function trashesAction()
+    {
+        if(!\core\db\models\user::IsSignedin()) return;
+        $this->layout->AddTitle("Home");
+        $f = new \core\db\models\folder();
+        if(!isset($this->request->params["directory"]))
+            $this->request->params["directory"] = 0;
+        $this->view->pid = $pid = $this->request->params["directory"];
+        $uid = \core\db\models\user::GetInstance()->user_id;
+        $this->view->folders = ($f->fetchTrashes($uid));
+        $n = new \core\db\models\note;
+        $this->view->notes = ($n->fetchTrashes($uid));
+        $l = new \core\db\models\link;
+        $this->view->links = ($l->fetchTrashes($uid));
+    }
+
+    /**
+    * The \modules\htmlModule\controllers\indexController::archivesAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function archivesAction()
+    {
+        
     }
 }
