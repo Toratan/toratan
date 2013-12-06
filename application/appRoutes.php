547d1429fbfe6b569090f313e74b266f1f15c463
dariush <b.g.dariush@gmail.com>
2013-12-06 19:41:18 +0330

    @ application/appRoutes.php: + $this->addRoute("^/(signin|signup|signout|recovery)$2", "/auth/$1$2");

--- application/appRoutes.php
+++ application/appRoutes.php
@@ -7,21 +7,22 @@ class appRoutes extends \zinux\kernel\routing\routerBootstrap
 {
     public function Fetch()
     {
-        $this->routeMessages();
-        $this->routeOpsView();
-    }
-    /**
-     * Routes {/message} to {/ops/message}
-     */
-    protected function routeMessages()
-    {
-        $this->addRoute("^/messages","/ops/messages");
-    }
-    /**
-     * Routes {/view} to {/ops/view}
-     */
-    protected function routeOpsView()
-    {
+        /**
+         * Routes 
+         *      {/view} to {/ops/view}
+         */
         $this->addRoute("^/view","/ops/view");
+        /**
+         * Routes 
+         *      {/message} to {/ops/message}
+         */
+        $this->addRoute("^/messages","/ops/messages");
+        /**
+         * Routes 
+         *      {/signin} to {/auth/signin}
+         *      {/signup} to {/auth/signup} 
+         *      {/signout} to {/auth/signout}
+         */
+        $this->addRoute("^/(signin|signup|signout|recovery)$2", "/auth/$1$2");
     }
 }
