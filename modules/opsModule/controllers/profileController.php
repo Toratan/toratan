2da8c8f531d45be09c9b2d0e2e46cf71c2dc71e6
dariush <b.g.dariush@gmail.com>
2013-12-29 12:45:06 +0330

    @ modules/opsModule/controllers/profileController.php: profile creatation is done

--- modules/opsModule/controllers/profileController.php
+++ modules/opsModule/controllers/profileController.php
@@ -7,12 +7,26 @@ namespace modules\opsModule\controllers;
  */
 class profileController extends \zinux\kernel\controller\baseController
 {
+    const PROFILE_NOT_CREATED = -1;
+    const PROFILE_SKIPPED = 0;
+    const PROFILE_CREATED = 1;
     /**
     * The modules\opsModule\controllers\profileController::IndexAction()
     * @by Zinux Generator <b.g.dariush@gmail.com>
     */
     public function IndexAction()
     {
+        $profile_status = \core\db\models\profile::getInstance(\core\db\models\user::GetInstance()->user_id)->getSetting("/profile/status") ;
+        # if profile not already created
+        if(!$profile_status || $profile_status == self::PROFILE_NOT_CREATED)
+        {
+            # invoke edit action
+            $this->editAction();
+            # set view to edit mode
+            $this->view->setView("edit");
+            # return from index action
+            return;
+        }
     }
 
     /**
@@ -71,7 +85,9 @@ class profileController extends \zinux\kernel\controller\baseController
                 # save on session cache socket
                 $sc->save("step#$current_step", $this->request->POST);
                 $this->submitProfile($sc);
-                break;
+                \core\db\models\profile::getInstance(\core\db\models\user::GetInstance()->user_id)->setSetting("/profile/status", self::PROFILE_CREATED);
+                header("location: /profile");
+                exit;
             # are we going to skip?
             case isset($this->request->params['skip']):
                 throw new \zinux\kernel\exceptions\notImplementedException("SKIP NOT IMPLEMENTED");
@@ -91,6 +107,22 @@ class profileController extends \zinux\kernel\controller\baseController
             throw new \zinux\kernel\exceptions\invalideArgumentException("NULL cache passed!!");
         # do whatever you want
         echo "<h1>Submiting profile with values: </h1>";
-        \zinux\kernel\utilities\debug::_var($session_cache->fetchAll(),1);
+        \zinux\kernel\utilities\debug::_var($session_cache->fetchAll());
+        $profile = \core\db\models\profile::getInstance(\core\db\models\user::GetInstance()->user_id);
+        foreach($session_cache->fetchAll() as $value)
+        {
+            if(!\is_array($value))
+                throw new \zinux\kernel\exceptions\invalideArgumentException("Invalid input! profile was not created....");
+            foreach($value as $key=> $_value)
+            {
+                if(!\is_array($_value))
+                    $profile->$key = $_value;
+                else
+                {
+                    $profile->$key = \implode(";", $_value);
+                }
+            }
+        }
+        $profile->save();
     }
 }
