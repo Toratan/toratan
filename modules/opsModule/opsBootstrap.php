d2b7ce495025eee1c8337f61cb23d98afa78137d
dariush <b.g.dariush@gmail.com>
2013-12-30 20:46:29 +0330

    working on profiling....

--- modules/opsModule/opsBootstrap.php
+++ modules/opsModule/opsBootstrap.php
@@ -17,8 +17,9 @@ class opsBootstrap
             return;
         # a list of exception {controller => action} which does not need signin sig.
         $signin_free_ops = array(
-           "index" => "view",
-           "index" => "explorer"
+            "index" => "view",
+            "index" => "explorer",
+            "profile" => "index"
         );
         # the normalized currently requested {conttroller => action} 
         $current_request = array(
