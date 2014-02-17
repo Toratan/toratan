f173b8c1e8ff11be83c271545227955cbdffa696
dariush <b.g.dariush@gmail.com>
2014-02-17 21:35:55 +0330

    @ application/dbBootstrap.php: toratan-script db configuration removed from project

--- application/dbBootstrap.php
+++ application/dbBootstrap.php
@@ -5,10 +5,9 @@ namespace application;
 */
 class dbBootstrap extends \zinux\kernel\application\applicationBootstrap
 {
-    const TORATAN = RUNNING_ENV;
-    const TORATAN_SCRIPT = "TORATAN_SCRIPTS";
-    /** 
-     * setups db using activerecord 
+    const MODE_TORATAN = RUNNING_ENV;
+    /**
+     * setups db using activerecord
      */
     public static function PRE_init_db()
     {
@@ -17,31 +16,26 @@ class dbBootstrap extends \zinux\kernel\application\applicationBootstrap
         {
             # fetching db related configurations
             $dbcfg = \zinux\kernel\application\config::GetConfig("toratan.db");
-            # fetching db related to scripts configurations
-            $dbcfg_scripts = \zinux\kernel\application\config::GetConfig("toratan.scripts.db");
             # setting connection string
             $cfg->set_connections(array(
-                    \application\dbBootstrap::TORATAN =>  
-                            "{$dbcfg["type"]}://{$dbcfg["username"]}:{$dbcfg["password"]}@{$dbcfg["host"]}/{$dbcfg["name"]}?charset=utf8",
-                    \application\dbBootstrap::TORATAN_SCRIPT => 
-                            "{$dbcfg_scripts["type"]}://{$dbcfg_scripts["username"]}:{$dbcfg_scripts["password"]}@{$dbcfg_scripts["host"]}/{$dbcfg_scripts["name"]}?charset=utf8")
+                    \application\dbBootstrap::MODE_TORATAN =>
+                            "{$dbcfg["type"]}://{$dbcfg["username"]}:{$dbcfg["password"]}@{$dbcfg["host"]}/{$dbcfg["name"]}?charset=utf8")
             );
-           # enable the connection string as to \application\dbBootstrap::TORATAN
-            $cfg->set_default_connection(\application\dbBootstrap::TORATAN);
+           # enable the connection string as to \application\dbBootstrap::MODE_TORATAN
+            $cfg->set_default_connection(\application\dbBootstrap::MODE_TORATAN);
         });
         # testing db connection
         \ActiveRecord\Connection::instance();
         # if we reach here we are all OK
     }
-    /** 
-     * switches database mode between {\application\dbBootstrap::TORATAN} <=> {\application\dbBootstrap::TORATAN_SCRIPTS}
+    /**
+     * switches database only to {\application\dbBootstrap::MODE_TORATAN}
      */
     public static function switch_database_mode($mode)
     {
         switch($mode)
         {
-            case self::TORATAN:
-            case self::TORATAN_SCRIPT:
+            case self::MODE_TORATAN:
                 \ActiveRecord\Config::instance()->set_default_connection($mode);
                 break;
             default:
