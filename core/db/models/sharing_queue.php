f1725150bef80d4bd06bc3dbd37104f190e79c2e
dariush <b.g.dariush@gmail.com>
2014-03-07 00:02:57 +0330

    + core/db/models/sharing_queue.php

---
+++ core/db/models/sharing_queue.php
@@ -0,0 +1,36 @@
+<?php
+namespace core\db\models;
+/**
+ * sharing_queue Entity
+ */
+class sharing_queue extends \core\db\models\baseModel 
+{
+    static $belongs_to= array("folder");
+    
+    public static function add_queue(\core\db\models\folder $folder)
+    {
+        if(!$folder)
+            throw new \zinux\kernel\exceptions\invalideArgumentException("The arg cannot be null!");
+        $sq = new \core\db\models\sharing_queue;
+        $sq->folder_id = $folder->folder_id;
+        try
+        {
+            $sq->save();
+        }
+        catch(\core\db\exceptions\alreadyExistsException $aee) { unset($aee); }
+    }
+    public static function read_queue()
+    {
+        $sq = new \core\db\models\sharing_queue;
+        $qitem = $sq->first(array("include"=>"folder"));
+        if(!$qitem) return NULL;
+        $folder = $qitem->folder;
+        $qitem->delete();
+        return $folder;
+    }
+    public static function is_empty()
+    {
+        $sq = new \core\db\models\sharing_queue;
+        return !$sq->first();
+    }
+}
