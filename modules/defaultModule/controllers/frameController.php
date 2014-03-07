b65b34292df2ce4de19e933f26d41d5c3db59144
dariush <b.g.dariush@gmail.com>
2014-03-07 14:00:59 +0330

    + modules/defaultModule/controllers/frameController.php

---
+++ modules/defaultModule/controllers/frameController.php
@@ -0,0 +1,17 @@
+<?php
+namespace modules\defaultModule\controllers;
+    
+/**
+ * The modules\defaultModule\controllers\frameController
+ * @by Zinux Generator <b.g.dariush@gmail.com>
+ */
+class frameController extends \zinux\kernel\controller\baseController
+{
+    /**
+    * The modules\defaultModule\controllers\frameController::IndexAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function IndexAction()
+    {
+    }
+}
