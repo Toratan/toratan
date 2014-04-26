a5ce66c7d55ce849006f6872615821707f8e4789
dariush <b.g.dariush@gmail.com>
2014-04-27 00:46:35 +0430

    + modules/opsModule/controllers/fetchController.php

---
+++ modules/opsModule/controllers/fetchController.php
@@ -0,0 +1,17 @@
+<?php
+namespace modules\opsModule\controllers;
+    
+/**
+ * The modules\opsModule\controllers\fetchController
+ * @by Zinux Generator <b.g.dariush@gmail.com>
+ */
+class fetchController extends \zinux\kernel\controller\baseController
+{
+    /**
+    * The modules\opsModule\controllers\fetchController::IndexAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function IndexAction()
+    {
+    }
+}
