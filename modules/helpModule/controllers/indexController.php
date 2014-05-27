7c6e4a5af4506be3c1fdb9c038db65ff63371c69
noise2 <b.g.dariush@gmail.com>
2014-05-27 21:54:41 +0430

    + modules/helpModule

---
+++ modules/helpModule/controllers/indexController.php
@@ -0,0 +1,17 @@
+<?php
+namespace modules\helpModule\controllers;
+    
+/**
+ * The modules\helpModule\controllers\indexController
+ * @by Zinux Generator <b.g.dariush@gmail.com>
+ */
+class indexController extends \zinux\kernel\controller\baseController
+{
+    /**
+    * The modules\helpModule\controllers\indexController::IndexAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function IndexAction()
+    {
+    }
+}
