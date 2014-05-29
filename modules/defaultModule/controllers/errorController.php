98787b1d0d9f905db74b4329ca78bb241170878d
noise2 <b.g.dariush@gmail.com>
2014-05-29 05:38:08 +0430

    @ modules/defaultModule/controllers/errorController.php: improved

--- modules/defaultModule/controllers/errorController.php
+++ modules/defaultModule/controllers/errorController.php
@@ -12,11 +12,14 @@ class errorController extends \zinux\kernel\controller\baseController
         parent::Initiate();
         $this->layout->SetLayout("error");
         $mp = new \zinux\kernel\utilities\pipe("__ERRORS__");
-        if(!$mp->hasFlow()) $this->view->error[] = new \Exception("!!NO ERROR!!");
-        while($mp->hasFlow()) {
-            $this->view->error[] = unserialize($mp->read());
+        if(!$mp->hasFlow()) {
+            header("location: /");
+            exit;
         }
-        $this->layout->AddTitle("Oops!");
+        # empty the pipe and only keep the last error up
+        while($mp->hasFlow())
+            $this->view->error =$mp->read();
+        $this->layout->AddTitle("Toratan");
     }
     /**
     * The modules\defaultModule\controllers\errorController::IndexAction()
@@ -24,41 +27,39 @@ class errorController extends \zinux\kernel\controller\baseController
     */
     public function IndexAction()
     {
-        if(count($this->view->error)) {
-            $last = end($this->view->error);
-            /** 
-             * status codes reference : RFC 2616
-             * http://tools.ietf.org/html/rfc2616
-             */
-            switch(true) {
-                case $last instanceof \zinux\kernel\exceptions\invalidCookieException:
-                case $last instanceof \zinux\kernel\exceptions\invalidArgumentException:
-                case $last instanceof \zinux\kernel\exceptions\invalidOperationException:
-                    $msg = "Bad Request";
-                    $code = 400;
-                    break;
-                case $last instanceof \zinux\kernel\exceptions\securityException:
-                case $last instanceof \zinux\kernel\exceptions\accessDeniedException:
-                case $last instanceof \zinux\kernel\exceptions\permissionDeniedException:
-                    $msg = "Forbidden";
-                    $code = 403;
-                    break;
-                case $last instanceof \zinux\kernel\exceptions\notFoundException:
-                case $last instanceof \core\db\exceptions\dbNotFoundException:
-                    $msg = "Not Found ";
-                    $code = 404;
-                    break;
-                default: 
-                    $msg = "Intername Server Error";
-                    $code  = 500;
-                    break;
-                case $last instanceof \zinux\kernel\exceptions\notImplementedException: 
-                    $msg = "Not Implemented";
-                    $code = 501; 
-                    break;
-            }
-            $this->view->setView("e$code");
-            header("HTTP/1.1 $code $msg");
+        $last = $this->view->error;
+        /** 
+         * status codes reference : RFC 2616
+         * http://tools.ietf.org/html/rfc2616
+         */
+        switch(true) {
+            case $last instanceof \zinux\kernel\exceptions\invalidCookieException:
+            case $last instanceof \zinux\kernel\exceptions\invalidArgumentException:
+            case $last instanceof \zinux\kernel\exceptions\invalidOperationException:
+                $msg = "Bad Request";
+                $code = 400;
+                break;
+            case $last instanceof \zinux\kernel\exceptions\securityException:
+            case $last instanceof \zinux\kernel\exceptions\accessDeniedException:
+            case $last instanceof \zinux\kernel\exceptions\permissionDeniedException:
+                $msg = "Forbidden";
+                $code = 403;
+                break;
+            case $last instanceof \zinux\kernel\exceptions\notFoundException:
+            case $last instanceof \core\db\exceptions\dbNotFoundException:
+                $msg = "Not Found ";
+                $code = 404;
+                break;
+            default: 
+                $msg = "Intername Server Error";
+                $code  = 500;
+                break;
+            case $last instanceof \zinux\kernel\exceptions\notImplementedException: 
+                $msg = "Not Implemented";
+                $code = 501; 
+                break;
         }
+        $this->view->setView("e$code");
+        header("HTTP/1.1 $code $msg");
     }
 }
