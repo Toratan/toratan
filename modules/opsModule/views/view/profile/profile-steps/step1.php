1a58dad6b17ca093468c4c924d15c0606874839c
noise2 <b.g.dariush@gmail.com>
2014-06-09 18:52:44 +0430

    @ modules/opsModule/views/view/profile/profile-steps/step1.php: a fix

--- modules/opsModule/views/view/profile/profile-steps/step1.php
+++ modules/opsModule/views/view/profile/profile-steps/step1.php
@@ -2,7 +2,7 @@
     div#step1 div#name-section div{float:left;}
     div#step1 div#birthday-section {padding-bottom: 20px;}
     div#step1 div#gender select,
-    div#step1 div#birthday-section select{float: left;margin-right: 20px;width: 130px}
+    div#step1 div#birthday-section select{float: left;margin-right: 20px;width: 130px; width: 200px}
     div string{margin: auto auto 1% auto}
     .row div{padding-right: 10%}
     .row{padding: 1%}
@@ -29,14 +29,17 @@
         <div class="">
             <select name="birth_month" class="form-control" required>
                 <option disabled="disabled" style="color:#e6e6e6">Month</option>
+                <option value='0'>Prefer not to disclose</option>
                 <?php $i=1; while($i<=12) echo "<option ".(($this->profile->birth_month==$i)?'selected="selected"':'')." value='$i'>".date('F',mktime(0,0,0,$i++))."</option>"; ?>
             </select>
             <select name="birth_day" class="form-control" required>
                 <option disabled="disabled" style="color:#e6e6e6">Day</option>
+                <option value='0'>Prefer not to disclose</option>
                 <?php $i=1; while($i<=31) echo "<option ".(($this->profile->birth_day==$i)?'selected="selected"':'')." value='$i'>".$i++."</option>"; ?>
             </select>
             <select name="birth_year" class="form-control" required>
                 <option disabled="disabled" style="color:#e6e6e6">Year</option>
+                <option value='0'>Prefer not to disclose</option>
                 <?php $i=date('Y'); while($i>=1900) echo "<option ".(($this->profile->birth_year==$i)?'selected="selected"':'')." value='$i'>".$i--."</option>"; ?>
             </select>
         </div>
@@ -46,7 +49,7 @@
     <div id="gender" class="clearfix" style="padding-bottom: 20px;">
         <label class="control-label"><strong class="">Gender</strong></label>
         <div class="">
-            <select name="is_male" class="form-control" style='width: 200px' required>
+            <select name="is_male" class="form-control" required>
                 <option <?php echo $this->profile->is_male == -1?'selected="selected"':''?> value="-1">Prefer not to disclose</option>
                 <option <?php echo $this->profile->is_male ==  1?'selected="selected"':''?> value="1">Male</option>
                 <option <?php echo !$this->profile->is_male?'selected="selected"':''?> value="0">Female</option>
