1924a882c716d4a7c2fbee7ced6935a25cec7dfb
noise2 <b.g.dariush@gmail.com>
2014-06-16 01:25:39 +0430

    + modules/apiModule/controllers/indexController.php

---
+++ modules/apiModule/controllers/indexController.php
@@ -0,0 +1,26 @@
+<?php
+namespace modules\apiModule\controllers;
+    
+/**
+ * The modules\apiModule\controllers\indexController
+ * @by Zinux Generator <b.g.dariush@gmail.com>
+ */
+class indexController extends \zinux\kernel\controller\baseController
+{
+    /**
+    * The modules\apiModule\controllers\indexController::IndexAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function IndexAction()
+    {
+    }
+
+    /**
+    * The \modules\apiModule\controllers\indexController::explorerAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function explorerAction()
+    {
+        
+    }
+}
