59720a71bbe83efc75d9d4ce1960b0fae97e52d3
noise2 <b.g.dariush@gmail.com>
2014-07-21 03:36:50 +0430

    + modules/opsModule/models/renderMessage.php

---
+++ modules/opsModule/models/renderMessage.php
@@ -0,0 +1,19 @@
+<?php
+namespace modules\opsModule\models;
+    
+/**
+* The modules\opsModule\models\renderMessage
+* @by Zinux Generator <b.g.dariush@gmail.com>
+*/
+class renderMessage
+{
+    /**
+     * The related view
+     * @var \zinux\kernel\view\baseView
+     */
+    protected $view;
+    public function __construct(\zinux\kernel\view\baseView $view) { $this->view = $view; }
+    public function __render_header() {
+        
+    }
+}
