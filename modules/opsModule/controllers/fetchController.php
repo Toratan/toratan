af6a64ae5fe89ee79111c5dfb9a5ea68a22f37ea
noise2 <b.g.dariush@gmail.com>
2014-07-26 05:14:17 +0430

    @ modules/opsModule/views/view/index/aceView.phtml: working on fetching tags

--- modules/opsModule/controllers/fetchController.php
+++ modules/opsModule/controllers/fetchController.php
@@ -7,27 +7,17 @@ namespace modules\opsModule\controllers;
  */
 class fetchController extends \zinux\kernel\controller\baseController
 {
-    const STEP_SIZE = 30;
-    public function Initiate()
-    {
-        parent::Initiate();
-        # init page count
-        if(!isset($this->request->params["page"]))
-            $this->request->params["page"] = 1;
-        # secure the request
-        \zinux\kernel\security\security::IsSecure($this->request->params, array("page", "item"));
-        # secure the request item
-        if(!in_array($this->request->params["item"], array("folder", "note", "link")))
-            throw new \zinux\kernel\exceptions\invalidArgumentException("Undefined `{$this->request->params["item"]}`");
-    }
     /**
     * The modules\opsModule\controllers\fetchController::IndexAction()
     * @by Zinux Generator <b.g.dariush@gmail.com>
     */
-    public function IndexAction()
-    {
-        $class = "\\core\\db\\models\\{$this->request->params["item"]}";
-        $instance = new $class;
-//        $instance = new \core\db\models\folder;
+    public function IndexAction() { throw new \zinux\kernel\exceptions\notImplementedException; }
+
+    /**
+    * The \modules\opsModule\controllers\fetchController::tagsAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function tagsAction() {
+        \zinux\kernel\utilities\debug::_var($this->request->params, 1);
     }
 }
