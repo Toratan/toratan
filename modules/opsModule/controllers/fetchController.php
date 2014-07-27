724ebc491ca580e53cb5876dc39bd61cb5b33168
noise2 <b.g.dariush@gmail.com>
2014-07-28 01:50:34 +0430

    @ modules/opsModule/views/view/fetch/tagsView.phtml: tag fetch is in effect

--- modules/opsModule/controllers/fetchController.php
+++ modules/opsModule/controllers/fetchController.php
@@ -18,10 +18,12 @@ class fetchController extends \zinux\kernel\controller\baseController
     * @by Zinux Generator <b.g.dariush@gmail.com>
     */
     public function tagsAction() {
+        if(!$this->request->IsPOST())
+            throw new \zinux\kernel\exceptions\accessDeniedException("invalid request type `{$_SERVER["REQUEST_METHOD"]}`");
         \zinux\kernel\security\security::__validate_request($this->request->params);
         \zinux\kernel\security\security::IsSecure($this->request->params, array("term"));
         $this->view->tags = \core\db\models\tag::search($this->request->params["term"]);
         $this->view->origin_term = $this->request->params["term"];
-        die;
+        $this->layout->SuppressLayout();
     }
 }
