682fa9e82572306450b62aac3c293905685d0ff4
noise2 <b.g.dariush@gmail.com>
2014-07-28 23:15:17 +0430

    @ core/db/models/tag.php: +::fetch_related_notes()

--- core/db/models/tag.php
+++ core/db/models/tag.php
@@ -4,8 +4,8 @@ namespace core\db\models;
 class tag extends baseModel
 {
     static $has_many = array(
-        array('note_tags', "class_name" => "\\core\\db\\models\\note_tag"),
-        array('notes', 'through' => '\\core\\db\\models\\note_tags')
+        array('note_tags', "class_name" => "note_tag"),
+        array('notes', 'through' => 'note_tags', "order" => "notes.popularity DESC")
     );
     public function save($validate=true)
     {
@@ -22,7 +22,7 @@ class tag extends baseModel
     public static function search($tag) {
         $builder = new \ActiveRecord\SQLBuilder(self::connection(), self::table_name());     
         $builder->select("*")->where("`tag_value` = ?", array($tag))->limit("1");
-        return self::find_by_sql($builder->to_s(), $builder->bind_values());
+        return array_shift(self::find_by_sql($builder->to_s(), $builder->bind_values()));
     }
     /**
      * Seach tags that is similar with passed tag's value
@@ -50,5 +50,23 @@ class tag extends baseModel
         }
         $t->readonly();
         return $t;
+    }
+    /**
+     * Fetch related notes that are bind to this tag
+     * @param integer $offset The query offset
+     * @param integer $limit The query limit(default: 10)
+     * @param string $order The query order(default: `popularity DESC`)
+     * @return array of array(count_of_all_notes_with_this_tag, current_result_due_to_offset_and_limit)
+     */
+    public function fetch_related_notes($offset, $limit = 10, $order = "popularity DESC") {
+        $tags = note_tag::all(array("conditions" => array("tag_id = ?", $this->tag_id), "offset" => $offset, "limit" => $limit, "select" => "note_id"));
+        $in = "";
+        foreach($tags as $tag) {
+            $in = "$in, '$tag->note_id'";
+        }
+        $in_val = trim($in, ", ");
+        $builder = new \ActiveRecord\SQLBuilder(note::connection(), note::table_name());
+        $builder->select("note_id, popularity")->where("note_id IN ($in_val)")->limit($limit)->offset($offset)->order($order);
+        return array(count($tags), note::find_by_sql($builder->to_s(), $builder->bind_values()));
     }
 }
