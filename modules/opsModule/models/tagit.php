752a1c988138a32dbdc4fc407599435377ccff7d
noise2 <b.g.dariush@gmail.com>
2014-07-28 04:46:11 +0430

    + modules/opsModule/models/tagit.php

---
+++ modules/opsModule/models/tagit.php
@@ -0,0 +1,19 @@
+<?php
+namespace modules\opsModule\models;
+    
+/**
+* The modules\opsModule\models\tagit
+* @by Zinux Generator <b.g.dariush@gmail.com>
+*/
+class tagit
+{
+    public function __renderJS(){
+        
+    }
+    public function __renderCSS(){
+        
+    }
+    public function __renderHTML(){
+        
+    }
+}
