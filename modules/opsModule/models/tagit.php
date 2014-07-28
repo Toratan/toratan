7f4b6eaf37cb6c2d83fd0c38382b7711e8ab2371
noise2 <b.g.dariush@gmail.com>
2014-07-28 05:44:43 +0430

    @ modules/opsModule/models/tagit.php: is in effect

--- modules/opsModule/models/tagit.php
+++ modules/opsModule/models/tagit.php
@@ -8,12 +8,139 @@ namespace modules\opsModule\models;
 class tagit
 {
     public function __renderJS(){
-        
+?>
+<script type='text/javascript'>
+        function close_tag_sug() {
+            $('.tag-sug-container')
+                .data('closed', 1)
+                .slideUp('fast');
+        }
+        function submit_tag(tag) {
+            $(".myTags").tagit("createTag", tag);
+            new_tag_focus();
+        }
+        function new_tag_focus() {
+            $(".myTags")
+                .data('uiTagit')
+                    .tagInput
+                        .focus();
+        }
+        function tagit(){
+            close_tag_sug();
+            $("ul.tagit").remove();
+            window.open_savecloseModal($("#tagit-container").html(), function(){
+                $("#tagit-container .myTags").val($(".modal-body .myTags").val());
+            });
+            setTimeout(function(){
+                new_tag_focus();
+                // to disable unblur submit tag event
+                $(".myTags")
+                    .data('uiTagit')
+                        .tagInput
+                            .unbind("blur");
+            }, 600);
+        };
+</script>
+<script src="/access/js/jquery-ui-1.11.0.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="/access/js/tag-it/js/tag-it.js" type="text/javascript" charset="utf-8"></script>
+<?php
     }
     public function __renderCSS(){
-        
+?>
+<style type="text/css">
+    .tag-sug-container {
+        position: fixed;
+        margin-top: -5px;
+        width: calc(100% - 41px);
+        height: auto;
+        border: 1px solid #000;
+        background-color: #FFF;
+        display: none;
+        padding:10px;
     }
-    public function __renderHTML(){
-        
+    .tagit-choice {
+        -moz-user-select: none; 
+        -khtml-user-select: none; 
+        -webkit-user-select: none; 
+        -o-user-select: none; 
+    }
+</style>
+<link href="/access/js/tag-it/css/jquery.tagit.css" rel="stylesheet" type="text/css">
+<link href="/access/js/tag-it/css/tagit.ui-zendesk.css" rel="stylesheet" type="text/css">
+<?php
+    }
+    public function __renderHTML($tags, $html_input_name = "tagit"){
+?>
+    <div id="tagit-container" style="display: none">
+        <label>Tag this note (Separate with SPACE):</label>
+        <div class='pull-right tag-fetching-wait' style='display: none'><img src='/access/img/ajax-loader.gif' alt="Searching..." title='Fetching similar tags....'/></div>
+        <div class="text-danger tag-error-overflow" style='font-weight: bold;margin-bottom: 10px;display: none'>
+            <span class='glyphicon glyphicon-warning-sign'></span> Tag's length cannot be more than 50 charactor!
+        </div>
+        <div class="text-danger tag-error-ajax" style='font-weight: bold;padding-bottom: 10px;display: none'></div>
+        <input name='<?php echo $html_input_name ?>' class='myTags' type="hidden" value='<?php echo $tags ?>'/>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $(".myTags").tagit({
+                    placeholderText: "Add a tag....",
+                    beforeTagRemoved: function(event, ui) {
+                        if(String(ui.tagLabel).trim().length === 0) return;
+                        $(ui.tag).remove();
+                    },
+                    beforeTagAdded: function(event, ui) {
+                        var retval = ui.tagLabel.length <= 50;
+                        if(!retval)
+                            $(".tag-error-overflow").fadeIn();
+                        else
+                            $(".tag-error-overflow").fadeOut();
+                        return retval;
+                    },
+                    afterTagAdded: function(event, ui) {
+                        close_tag_sug();
+                    }
+                }).data('uiTagit')
+                        .tagInput
+                            .keyup(function(e){
+                                clearTimeout($(this).data("autocomplete_delay"));
+                                close_tag_sug();
+                                if($(this).data("autocomplete_prev_txt") === $(this).val() || $(this).val().trim().length === 0) return;
+                                clearTimeout($(this).data("autocomplete_delay"));
+                                var val = $(this).val();
+                                var $this = $(this);
+                                $(this).data("autocomplete_delay", setTimeout(function(){
+                                    $this.data("autocomplete_prev_txt", val);
+                                    $(".tag-fetching-wait").show();
+                                    $(".tag-error-ajax").html("").slideUp();
+                                    $.ajax({
+                                        url: "/ops/fetch/tags?<?php echo \zinux\kernel\security\security::__get_uri_hash_string() ?>&term="+val,
+                                        type: "POST",
+                                        global: false,
+                                        success: function(data) {
+                                            $(".tag-sug-container")
+                                                .html(data)
+                                                .slideDown();
+                                        }
+                                    }).complete(function(){
+                                        $(".tag-fetching-wait").fadeOut();
+                                    }).fail(function(xhr) {
+                                        var info = xhr.responseText;
+                                        var $body = $('<div>').append($(info)).find("#error-layout").wrap("<div>");
+                                        if($body.length !== 0) {
+                                            $body.find(".container").removeClass("container").css("margin", "-10px").html();
+                                            info = $body.html();
+                                        }
+                                        $(".tag-error-ajax").html(info).fadeIn();
+                                    });
+                                }, 500));
+                            })
+                            // to disable unblur submit tag event
+                            .unbind("blur");
+            });
+        </script>
+        <div class="tag-sug-container">
+            <a href='#' style='position: absolute;bottom: 0px;right: 10px;' class="close-tag-sug block" onclick="close_tag_sug(); return false;">Close</a>
+        </div>
+    </div>
+<?php
     }
 }
