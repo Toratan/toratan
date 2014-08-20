59cc0795c6de3cc07939c022a21af0e0d76834c5
noise2 <b.g.dariush@gmail.com>
2014-08-20 11:24:38 +0430

    @ modules/opsModule/models/noteViewModel.php: commenting UI is almost done

--- modules/opsModule/models/noteViewModel.php
+++ modules/opsModule/models/noteViewModel.php
@@ -335,25 +335,44 @@ class noteViewModel
 <style type="text/css">
     .comments-container {padding: 20px 40px 20px 40px}
     .comments-container > *:not(.clearfix) {margin-bottom: 10px}
+    .comments-container .avatar img { -webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px; }
     .comments-container .total-comment-no { font-weight: bold; font-size: large }
     .comments-container .total-comment-no label { font-weight: bold; font-size: medium;}
-    .comments-container .prev-comment-area .comments-head {border-bottom: 2px solid #EEE;}
+    .comments-container .prev-comment-area .comments-head {border-bottom: 2px solid #EEE;margin-bottom: 20px}
     .comments-container .prev-comment-area .comments-head li{ width: 70px;padding: 5px}
     .comments-container .prev-comment-area .comments-head li a {text-decoration: none;font-weight: bold;color:#7c7c7c}
     .comments-container .prev-comment-area .comments-head li.active a {font-weight: bolder;color:#000}
-    .comments-container .prev-comment-area .comments-head li:not(.active) a {display: block;}
+    .comments-container .prev-comment-area .comments-head li a {display: block;}
     .comments-container .prev-comment-area .comments-head li:not(.active) a:hover{color:#5a5a5a}
     .comments-container .prev-comment-area .comments-head li:not(.active):hover { border-bottom: 2px solid #ffcc44;}
     .comments-container .prev-comment-area .comments-head li.active{ border-bottom: 2px solid #0088cc}
     .comments-container .prev-comment-area .comments-head li .careta{ margin-top: 10px;margin-left: 3px;}
     .comments-container .prev-comment-area .comments-head li:not(.active) .caret{ visibility: collapse;}
+    .comments-container .user-comment-erea .form-control { -webkit-border-radius: 0; -moz-border-radius: 0; border-radius: 0; }
+    .comments-container .prev-comment-area .comment {margin-bottom: 30px}
+    .comments-container .prev-comment-area .comment .comment-body {padding-left: 30px}
+    .comments-container .prev-comment-area .comment .comment-body .commenter-detail .commenter-link {font-weight: bolder; display: inline}
+    .comments-container .prev-comment-area .comment .comment-body .commenter-detail .comment-date {display: inline;color:#AAA;font-weight: bold;font-size: small}
+    .comments-container .prev-comment-area .comment .comment-body .commenter-detail .comment-date::before {content: " . "}
+    .comments-container .prev-comment-area .comment .comment-body .comment-data {margin: 10px; overflow: auto;}
+    .comments-container .prev-comment-area .comment .comment-body .comment-footer {margin: 0 10px ;}
+    .comments-container .prev-comment-area .comment .comment-body .comment-footer .vote { text-decoration: none}
+    .comments-container .prev-comment-area .comment .comment-body .comment-footer .vote:hover { font-weight: bold}
+    .comments-container .prev-comment-area .comment .comment-body .comment-footer > * {display: inline;padding: 3px;}
+    .comments-container .prev-comment-area .comment .comment-body .comment-footer .divider {padding-top: -10px}
+    .comments-container .prev-comment-area .comment .comment-body .comment-footer .divider::after {content: "."}
+    .comments-container .prev-comment-area .comment .comment-body .comment-footer .actions * {padding: 0;margin: 0}
+    .comments-container .prev-comment-area .comment .comment-body .comment-footer .actions a {color:#0088cc;padding: 2px;}
+    .comments-container .prev-comment-area .load-more-comment {border: 1px solid #ddd;height: 45px;padding-top: 10px}
     @media screen and (min-width:0px) and (max-width: 399px) { 
         .comments-container .user-comment-erea .comment-signin-container .burden{ display: none}
     }
-    
+    @media screen and (min-width:0px) and (max-width: 700px) {
+        .comments-container .prev-comment-area .comment .comment-body {padding-left: 10px}
+    }
 </style>
 <div class="row">
-    <div class="col-md-push-2 col-md-8 comments-container" style="border: 1px solid #000;min-height: 100px">
+    <div class="col-md-push-2 col-md-8 comments-container" astyle="border: 1px solid #000;min-height: 100px">
         <div  class="total-comment-no">15 <label>comments</label></div>
         <div class="user-comment-erea" style="width: 100%">
             <div class="row">
@@ -362,12 +381,18 @@ class noteViewModel
                     <span class="glyphicon glyphicon-warning-sign"></span> <span class="burden">You must </span><a href="/signup">Signup</a> or <a href="/signin">Signin</a> to comment.
                 </div>
 <?php else: ?>
-                <div class="hidden-xs col-sm-1" sstyle="height: 50px;border: 1px solid #000;padding: 0">
+                <div class="hidden-xs col-sm-1 avatar" sstyle="height: 50px;border: 1px solid #000;padding: 0">
                     <?php list($avatar_uri , $def_avatar) = \core\ui\html\avatar::get_avatar_link(\core\db\models\user::GetInstance()->user_id); ?>
                     <img src="<?php echo $avatar_uri ?>" onerror="this.src='<?php echo $def_avatar ?>'" height="50" width="50">
                 </div>
                 <div class="col-xs-12 col-sm-11" sstyle="height: 50px;border: 1px solid #000">
-                    <textarea class="form-control" style="margin-top: -3px;max-width: 100%" placeholder="Leave a comment...."></textarea>
+                    <textarea class="form-control" name="comment" style="margin-top: -3px;max-width: 100%" placeholder="Leave a comment...."></textarea>
+                    <script src="/access/js/autosize/jquery.autosize.min.js" type="text/javascript"></script>
+                    <script type="text/javascript">
+                        (function(){
+                            $("textarea[name='comment']").autosize();
+                        })(jQuery);
+                    </script>
                 </div>
 <?php endif; ?>
             </div>
@@ -378,7 +403,47 @@ class noteViewModel
                 <li class="active"><a href="#">Best  <span class='caret'></span></a></li>
                 <li><a href="#">All <span class='caret'></span></a></li>
             </ul>
-            <div style="min-height: 400px;"></div>
+            <div class="comments">
+                <?php for($i=0;$i<3;$i++) : ?>
+                <div class="comment">
+                    <div class="row">
+                        <div class="hidden-xs col-sm-1 comment-header avatar">
+                            <img src="<?php echo $avatar_uri ?>" onerror="this.src='<?php echo $def_avatar ?>'" height="50" width="50">
+                        </div>
+                        <div class="col-xs-12 col-sm-11 pull-right comment-body">
+                            <div class="row">
+                                <div class="col-xs-12 commenter-detail">
+                                    <div class="commenter-link">
+                                        <?php $commenter = \core\db\models\user::GetInstance() ?>
+                                        <a href="/@<?php echo $commenter->username ?>"><?php echo $commenter->get_RealName_or_Username() ?></a>
+                                    </div>
+                                    <div class="comment-date">3 minutes ago</div>
+                                </div>
+                                <div class="col-xs-12 comment-data">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sodales est dui, sed posuere erat porta vehicula. In sit amet ligula et neque commodo condimentum. Suspendisse vulputate condimentum nisi, ac pulvinar tellus consectetur et. Donec malesuada urna egestas vestibulum hendrerit.
+                                </div>
+                                <div class="col-xs-12 comment-footer">
+                                    <a href="#" class="vote vote-up text-success">2 <span class="glyphicon glyphicon-chevron-up"></span></a>
+                                    <div class="divider"></div>
+                                    <a href="#" class="vote vote-down text-danger">4 <span class="glyphicon glyphicon-chevron-down"></span></a>
+                                    <div class="actions pull-right">
+                                        <ul class="list-inline">
+                                            <li><a href="#">Edit</a></li>
+                                            <li class="divider"></li>
+                                            <li><a href="#">Delete</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <?php endfor; ?>
+            </div>
+            <div class='load-more-comment text-center'>
+                <a href="#">Load more comments.</a>
+            </div>
         </div>
         <div class="clearfix"></div>
     </div>
