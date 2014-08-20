16126bec090a3060e3f20f95d8ab5fb2e324ba1d
noise2 <b.g.dariush@gmail.com>
2014-08-20 13:36:07 +0430

    + modules/opsModule/controllers/commentController.php

---
+++ modules/opsModule/controllers/commentController.php
@@ -0,0 +1,44 @@
+<?php
+namespace modules\opsModule\controllers;
+/**
+ * The modules\opsModule\controllers\commentController
+ * @by Zinux Generator <b.g.dariush@gmail.com>
+ */
+class commentController extends \zinux\kernel\controller\baseController
+{
+    /**
+    * The modules\opsModule\controllers\commentController::IndexAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function IndexAction()
+    {
+    }
+    /**
+    * The \modules\opsModule\controllers\commentController::newAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function newAction()
+    {
+    }
+    /**
+    * The \modules\opsModule\controllers\commentController::editAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function editAction()
+    {
+    }
+    /**
+    * The \modules\opsModule\controllers\commentController::voteAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function voteAction()
+    {
+    }
+    /**
+    * The \modules\opsModule\controllers\commentController::markAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function markAction()
+    {
+    }
+}
