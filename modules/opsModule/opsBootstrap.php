8e78da81061e2590ae15578c033d147bf4138fb1
noise2 <b.g.dariush@gmail.com>
2014-08-24 12:05:44 +0430

    @ modules/opsModule/opsBootstrap.php: its performance optimized for checking unsigned clients pass-through check point

--- modules/opsModule/opsBootstrap.php
+++ modules/opsModule/opsBootstrap.php
@@ -15,44 +15,27 @@ class opsBootstrap
         if(\core\db\models\user::IsSignedin())
             # no need for check
             return;
-        # a list of exception array({controller => action}) which does not need signin sig.
+        # a list of exception array({controller => array(actions)}) which does not need signin sig.
         $signin_free_ops = array(
-            array("index", "view"),
-            array("index", "goto"),
-            array("index", "explorer"),
-            array("profile", "index"),
-            array("profile", "about"),
-            array("profile", "posts"),
-            array("profile", "avatar_view"),
-            array("fetch", "comment"),
-            array("fetch", "popular"),
+            "index"  => array("view", "goto", "explorer"),
+            "profile" => array("index", "about", "posts", "avatar_view"),
+            "fetch"   => array("comment", "popular")
         );
         # the normalized currently requested {conttroller => action} 
         $current_request = array(
             # the pure controller name without the suffix
-            strtolower($request->controller->name), 
+            "controller" => strtolower($request->controller->name), 
             # the pure action name without the suffix 
-            strtolower($request->action->name)
+            "action"       => strtolower($request->action->name)
         );
         # if current request's matches with an index in the signin free list
-        foreach ($signin_free_ops as $pair)
-        {
-            if(!is_array($pair)) throw new \zinux\kernel\exceptions\invalidOperationException("expecting `pair` to be array");
-            $controller = $pair[0];
-            $action = $pair[1];
-            # proceed to checking actions
-            if(
-                strtolower($controller) == $current_request[0] &&
-                strtolower($action) == $current_request[1]
-            )
-                # if it matches, we are OK
-                # no need to signin
-                return;
-        }
-        # if the PC didn't changed in previous lines
+        if(isset($signin_free_ops[$current_request["controller"]]) && in_array($current_request["action"], $signin_free_ops[$current_request["controller"]]))
+            return;
+        # otherwise
+__MUST_SIGNIN:
         # we need to signin 
         # we will also pass the continue spot to auth module
-        header("location: /auth/signin?continue={$request->GetURI()}");
+        header("location: /auth/signin?continue={$request->GetPrimaryURI()}");
         # halt the PHP
         exit;
     }
