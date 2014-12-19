052c34ef92bbe78d54cb43f3de9720cfb8ff6d2a
noise2 <b.g.dariush@gmail.com>
2014-12-19 20:23:05 +0330

    @ testing auth

--- modules/authModule/controllers/indexController.php
+++ modules/authModule/controllers/indexController.php
@@ -369,6 +369,58 @@ __CHECK_ERROR:
     */
     public function oauth2callbackAction()
     {
-        \zinux\kernel\utilities\debug::_var($this->request->params, 1);
+        (new \vendor\gAPI\googleAPIInitializer)->Execute();
+        $client_id = '607900320764-1utavhv74k7np0h62c4n47c6pp73t9ap.apps.googleusercontent.com';
+        $client_secret = 'ii61rYzQt1W_gvd1ga01avr5';
+        $redirect_uri = 'http://www.toratan.org/auth/oauth2callback';
+        $simple_api_key = 'AIzaSyCtY6WfVxQ2T3C-Jja9E7eX-RJzPu6NtDY';
+
+        //Create Client Request to access Google API
+        $client = new \Google_Client();
+        $client->setApplicationName("PHP Google OAuth Login Example");
+        $client->setClientId($client_id);
+        $client->setClientSecret($client_secret);
+        $client->setRedirectUri($redirect_uri);
+        $client->setDeveloperKey($simple_api_key);
+        $client->addScope("https://www.googleapis.com/auth/plus.me");
+        $client->addScope("https://www.googleapis.com/auth/userinfo.email");
+        $client->addScope("https://www.googleapis.com/auth/userinfo.profile");
+
+        //Send Client Request
+        $objOAuthService = new \Google_Service_Oauth2($client);
+
+        //Logout
+        if (isset($_REQUEST['logout'])) {
+          unset($_SESSION['access_token']);
+          $client->revokeToken();
+          header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL)); //redirect user back to page
+        }
+
+        //Authenticate code from Google OAuth Flow
+        //Add Access Token to Session
+        if (isset($_GET['code'])) {
+          $client->authenticate($_GET['code']);
+          $_SESSION['access_token'] = $client->getAccessToken();
+          header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+        }
+
+        //Set Access Token to make Request
+        if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+          $client->setAccessToken($_SESSION['access_token']);
+        }
+
+        //Get User Data from Google Plus
+        //If New, Insert to Database
+        if ($client->getAccessToken()) {
+          $userData = $objOAuthService->userinfo->get();
+          if(!empty($userData)) {
+              \zinux\kernel\utilities\debug::_var($userData);
+          }
+          $_SESSION['access_token'] = $client->getAccessToken();
+        } else {
+          $authUrl = $client->createAuthUrl();
+        }
+        \zinux\kernel\utilities\debug::_var(array($authUrl, $this->request->params));
+        die("<a href='$authUrl'>Auth</a>");
     }
 }
