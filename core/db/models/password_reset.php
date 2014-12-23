bff70c38b8eff3431061b0c9a17045cb7cf7496c
noise2 <b.g.dariush@gmail.com>
2014-12-23 15:51:44 +0330

    @ core/db/models/password_reset.php: a fix

--- core/db/models/password_reset.php
+++ core/db/models/password_reset.php
@@ -3,10 +3,12 @@ namespace core\db\models;
 
 class password_reset extends \core\db\models\baseModel
 {
-    public static function __create_request_id($email) {
+    public static function __create_request_id($email, $second_to_expire = 0) {
         $ins = new self;
         $ins->password_reset_id =\zinux\kernel\security\hash::Generate($email);
         $ins->user_email = $email;
+        if($second_to_expire)
+            $ins->expires_at = time() + $second_to_expire;
         $ins->save();
         return $ins->password_reset_id;
     }
