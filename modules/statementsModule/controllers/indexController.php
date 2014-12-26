6864589e12674e8ade638f087482bffcc3c4ec8b
noise2 <b.g.dariush@gmail.com>
2014-12-26 16:27:29 +0330

    + modules/statementsModule/

---
+++ modules/statementsModule/controllers/indexController.php
@@ -0,0 +1,54 @@
+<?php
+namespace modules\statementsModule\controllers;
+    
+/**
+ * The modules\statementsModule\controllers\indexController
+ * @by Zinux Generator <b.g.dariush@gmail.com>
+ */
+class indexController extends \zinux\kernel\controller\baseController
+{
+    /**
+    * The modules\statementsModule\controllers\indexController::IndexAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function IndexAction()
+    {
+    }
+
+    /**
+    * The \modules\statementsModule\controllers\indexController::aboutAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function aboutAction()
+    {
+        
+    }
+
+    /**
+    * The \modules\statementsModule\controllers\indexController::termsAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function termsAction()
+    {
+        
+    }
+
+    /**
+    * The \modules\statementsModule\controllers\indexController::privacyAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function privacyAction()
+    {
+        
+    }    
+
+    /**
+    * The \modules\statementsModule\controllers\indexController::cookiesAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function cookiesAction()
+    {
+        
+    }
+}
+
