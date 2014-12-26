56cc999960ff223b968eb8821de645d1c5a4cdce
noise2 <b.g.dariush@gmail.com>
2014-12-26 16:59:27 +0330

    @ modules/statementsModule/controllers/indexController.php: privacy is updated

--- modules/statementsModule/controllers/indexController.php
+++ modules/statementsModule/controllers/indexController.php
@@ -1,54 +1,35 @@
 <?php
 namespace modules\statementsModule\controllers;
-    
 /**
  * The modules\statementsModule\controllers\indexController
  * @by Zinux Generator <b.g.dariush@gmail.com>
  */
 class indexController extends \zinux\kernel\controller\baseController
 {
+    public function Initiate() { parent::Initiate(); $this->layout->AddTitle(ucwords($this->request->action->name)." - Toratan"); }
     /**
     * The modules\statementsModule\controllers\indexController::IndexAction()
     * @by Zinux Generator <b.g.dariush@gmail.com>
     */
-    public function IndexAction()
-    {
-    }
-
+    public function IndexAction() { throw new \zinux\kernel\exceptions\notImplementedException; }
     /**
     * The \modules\statementsModule\controllers\indexController::aboutAction()
     * @by Zinux Generator <b.g.dariush@gmail.com>
     */
-    public function aboutAction()
-    {
-        
-    }
-
+    public function aboutAction() { throw new \zinux\kernel\exceptions\notImplementedException; }
     /**
     * The \modules\statementsModule\controllers\indexController::termsAction()
     * @by Zinux Generator <b.g.dariush@gmail.com>
     */
-    public function termsAction()
-    {
-        
-    }
-
+    public function termsAction() { throw new \zinux\kernel\exceptions\notImplementedException; }
     /**
     * The \modules\statementsModule\controllers\indexController::privacyAction()
     * @by Zinux Generator <b.g.dariush@gmail.com>
     */
-    public function privacyAction()
-    {
-        
-    }    
-
+    public function privacyAction() { }
     /**
     * The \modules\statementsModule\controllers\indexController::cookiesAction()
     * @by Zinux Generator <b.g.dariush@gmail.com>
     */
-    public function cookiesAction()
-    {
-        
-    }
+    public function cookiesAction() { throw new \zinux\kernel\exceptions\notImplementedException; }
 }
-
--- modules/statementsModule/statementsBootstrap.php
+++ modules/statementsModule/statementsBootstrap.php
@@ -3,20 +3,4 @@ namespace modules\statementsModule;
 /**
 * The statementsModule's Bootstrapper
 */
-class statementsBootstrap
-{
-    /**
-     * A pre-dispatch function
-     * @param \zinux\kernel\routing\request $request
-     */
-    public function pre_CHECK(\zinux\kernel\routing\request $request)
-    {
-    }
-    /**
-     * A post-dispatch function
-     * @param \zinux\kernel\routing\request $request
-     */
-    public function post_CHECK(\zinux\kernel\routing\request $request)
-    {
-    }
-}
+class statementsBootstrap { }
